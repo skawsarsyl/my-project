e7e45e0a69375fba997ada0a7a0b88c036c6906f
Shahjahan Kawsar <skawsarsyl@gmail.com>
2026-07-05 00:38:48 +0600

    Back to Top Button

--- script.js
+++ script.js
@@ -69,3 +69,36 @@ setTimeout(erase,45);
 }
 
 document.addEventListener("DOMContentLoaded",type);
+
+/* ===============================
+   Back To Top Button
+=============================== */
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 400){
+
+        backToTop.classList.add("show");
+
+    }else{
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
+
